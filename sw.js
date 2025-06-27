6679874978f0e7b55556e85788cdf82ed9ff635a
Akash Choudhary <126048612+akashsodh@users.noreply.github.com>
2025-06-27 23:19:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // **महत्वपूर्ण:** जब भी आप कोई टेस्ट सीरीज़ या कोई और फाइल अपडेट करें,
 // तो इस संस्करण को बदलें (जैसे, 'smartbook-v2', 'smartbook-v4', आदि)
-const cacheName = 'smartbook-v32';
+const cacheName = 'smartbook-v33';
 
 // वे सभी फाइलें जिन्हें आप ऑफलाइन उपलब्ध कराना चाहते हैं
 const assets = [
@@ -24,6 +24,7 @@ const assets = [
     '/lib_unit3.json',
     '/lib_unit4.json',
     '/sood_mcq.json',
+    '/libpyq.json',
     '/images/icons/icon-72x72.png',
     '/images/icons/icon-96x96.png',
     '/images/icons/icon-128x128.png',
